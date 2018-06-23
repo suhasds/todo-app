7536acc57f004455805e5563cb4a30d444516e73
Suhas DS <suhas@totalcloud.io>
2018-06-23 16:09:51 +0530

    todo app creation

--- index.js
+++ index.js
@@ -1,43 +1,105 @@
-var express = require("express");
-var fs = require("fs");
+let express = require("express");
 let path = require("path");
+let methodOverride = require("method-override");
 
-
-var app = express();
-app.set('view engine', 'pug')
+let app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(
+  methodOverride(function(req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-const users = [
-    {name: "Suhas"},
-    {name: "Vijay"},
-    {name: "arun"},
-    {name: "ankur"}
-]
+let todos = [
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Pick up milk",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Learn templating",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Learn loops",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Learn about extending layouts",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Refactor route handlers to controller",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Refactor route handlers to a controller",
+    completed: false,
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Use Twitter Bootstrap to make things look a little better",
+    completed: false,
+  },
+];
 
+app.get("/todos", function(req, res) {
+  res.render("todos", {
+    todos: todos,
+  });
+});
 
-app.get("/", function(req, res){
-    // let indexFile = fs.readFileSync("./index.html");
-    // res.render(__dirname + '/index.pug');
-    // let pathString = path.join(__dirname, "/views/index.pug");
-    res.render("index", {
-        message: req.query.message,
-        foo: req.query.foo,
-        bar: req.query.bar
-    });
-})
+app.post("/todos", function(req, res) {
+  let todo = {
+    id: Math.floor(Math.random() * 100),
+    title: req.body.title,
+    completed: false,
+  };
 
+  todos.push(todo);
 
-app.get("/users", function(req, res){
-    res.json(users);
-})
+  res.redirect("/todos");
+});
 
-app.post("/users", function(req, res){
-    // res.send(req.body);
-    users.push({name: req.body.name});
-    res.json(users);
-})
+app.put("/todos/:id", function(req, res) {
+  let id = req.params.id;
+
+  todos.forEach(function(todo) {
+    if (todo.id == id) {
+      todo.completed = req.body.hasOwnProperty("todo");
+    }
+  });
+
+  return res.redirect("/todos");
+});
+
+app.delete("/todos/:id", function(req, res) {
+  let id = req.params.id;
+  todos = todos.filter(function (todo) {
+    if (todo.id == req.params.id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  res.redirect("/todos");
+});
 
 app.listen(3000);
-
-console.log("listening on port 3000");
